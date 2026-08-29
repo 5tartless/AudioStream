edeef68489e1d23cdc8badd5c99703848f470067
5tartless <sayangprooground@gmail.com>
2026-08-29 17:04:49 -0600

    fix2

--- src/backend/network/watcher.py
+++ src/backend/network/watcher.py
@@ -17,7 +17,9 @@ class ConnectionWatcher():
         self.watcher_thread.start()
     def stop(self) -> None:
         self.running = False
-        self.watcher_thread.join()
+        self.watcher_thread: threading.Thread
+        if self.watcher_thread.is_alive():
+            self.watcher_thread.join()
         self.reset()
 
     def reset(self):
